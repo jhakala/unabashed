77522f2204c33991964578d9ed8079c843588b9c
John Hakala <john_hakala@brown.edu>
2016-04-13 03:49:40 -0400

    update dbtunnel script for updated db

--- dbtunnel.sh
+++ dbtunnel.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ssh -t -L10121:localhost:10121 lxplus.cern.ch "ssh -L 10121:cmsrac21:10121 cmsusr"
+ssh -t -L10121:localhost:10121 lxplus.cern.ch 'ssh -t -L10121:lxplus.cern.ch:10121 cmsusr "ssh -D10121 cmsrac31-v"'
+#ssh -NL:10121:cmsrac31-v:10121 cmsusr
