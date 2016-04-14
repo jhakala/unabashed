0a6e9dfe8b96f648683390a2b5997f2ed2114537
John Hakala <johakala@johns-macbook-pro.home>
2016-04-14 19:28:39 +0200

    it works now

--- dbtunnel.sh
+++ dbtunnel.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# this is for connecting to the RSDB for p5 from either inside CERN or outside
+# usage: ./dbtunnel.sh outside
+# John Hakala 4/14/2016
 if [ $1 = "outside" ]; then
-	echo "this might not work yet..."
-	#ssh lxplus.cern.ch -L10121:localhost:10121 'ssh -L:10121:cmsrac31-v:10121 cmsusr.cern.ch'
 	ssh -Y -t -f lxplus.cern.ch -L10121:localhost:10121 "ssh -Y -NL10121:cmsrac31-v:10121 cmsusr" 
 elif [ $1 = "cern" ]; then
 	ssh -Y -NL:10121:cmsrac31-v:10121 cmsusr.cern.ch
