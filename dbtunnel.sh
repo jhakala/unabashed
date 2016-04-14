fa967a29771c13f19cae062d4f61c346203d79c7
John Hakala <johakala@johns-macbook-pro.home>
2016-04-14 19:25:59 +0200

    some fixes for new computer, connect to p5RSdb from outside

--- dbtunnel.sh
+++ dbtunnel.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-ssh -t -L10121:localhost:10121 lxplus.cern.ch 'ssh -t -L10121:lxplus.cern.ch:10121 cmsusr "ssh -D10121 cmsrac31-v"'
-#ssh -NL:10121:cmsrac31-v:10121 cmsusr
+if [ $1 = "outside" ]; then
+	echo "this might not work yet..."
+	#ssh lxplus.cern.ch -L10121:localhost:10121 'ssh -L:10121:cmsrac31-v:10121 cmsusr.cern.ch'
+	ssh -Y -t -f lxplus.cern.ch -L10121:localhost:10121 "ssh -Y -NL10121:cmsrac31-v:10121 cmsusr" 
+elif [ $1 = "cern" ]; then
+	ssh -Y -NL:10121:cmsrac31-v:10121 cmsusr.cern.ch
+fi
