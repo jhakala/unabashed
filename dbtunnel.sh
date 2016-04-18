f17f5bc5ff4ff608e41c7d04de41b3cb919bf796
John Hakala <john_hakala@brown.edu>
2016-04-18 10:29:57 +0200

    update tunnel scripts

--- dbtunnel.sh
+++ dbtunnel.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# this is for connecting to the RSDB for p5 from either inside CERN or outside
-# usage: ./dbtunnel.sh outside
+# this is for connecting to the RSDB for p5 or 904 from either inside CERN or outside
+# usage: ./dbtunnel.sh outside cms
 # John Hakala 4/14/2016
-if [ $1 = "outside" ]; then
-	ssh -Y -t -f lxplus.cern.ch -L10121:localhost:10121 "ssh -Y -NL10121:cmsrac31-v:10121 cmsusr" 
-elif [ $1 = "cern" ]; then
-	ssh -Y -NL:10121:cmsrac31-v:10121 cmsusr.cern.ch
+if [ $2 = "cms" ]; then
+	if [ $1 = "outside" ]; then
+		ssh -Y -t -f lxplus.cern.ch -L10121:localhost:10121 "ssh -Y -NL10121:cmsrac31-v:10121 cmsusr" 
+	elif [ $1 = "cern" ]; then
+		ssh -Y -NL:10121:cmsrac31-v:10121 cmsusr.cern.ch
+	fi
+elif [ $2 = "904" ]; then
+	if [ $1 = "outside" ]; then
+		ssh -Y -t -f lxplus.cern.ch -L10121:localhost:10121 "ssh -Y -NL10121:int2r1-v.cern.ch:10121 cms904usr" 
+	elif [ $1 = "cern" ]; then
+		ssh -Y -NL10121:int2r1-v.cern.ch:10121 cms904usr.cern.ch
+	fi
 fi
--- outsidetunnel.sh
+++ outsidetunnel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-expect -c 'eval spawn ssh -t -f lxplus.cern.ch -L1081:localhost:1081 "ssh -t -f -ND 1081 cmsusr" &; interact'
-expect -c 'eval spawn ssh -t -f lxplus.cern.ch -L1080:localhost:1080 "ssh -t -f -ND 1080 cms904usr" &; interact'
+expect -c 'eval spawn ssh -Y -t -f lxplus.cern.ch -L1081:localhost:1081 "ssh -t -f -ND 1081 cmsusr" &; interact'
+expect -c 'eval spawn ssh -Y -t -f lxplus.cern.ch -L1080:localhost:1080 "ssh -t -f -ND 1080 cms904usr" &; interact'
