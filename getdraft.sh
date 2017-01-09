6d3f65ce0ca824fba786341a61bf18795240e736
John Hakala <john_hakala@brown.edu>
2017-01-09 14:18:37 -0600

    getdraft.sh

--- getdraft.sh
+++ getdraft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DATE=`date  +%m-%d-%y`
 ITERATION=1
-while [ -f ./AN_draft_${DATE}_v${ITERATION}.pdf ];
+while [ -f ~/HgammaAN/drafts/AN_draft_${DATE}_v${ITERATION}.pdf ];
   do ITERATION=$(($ITERATION + 1))
 done
-scp johakala@lxplus.cern.ch:/afs/cern.ch/work/j/johakala/private/HgammaSVN/notes/tmp/AN-16-125_temp.pdf ./AN_draft_${DATE}_v${ITERATION}.pdf
+scp johakala@lxplus.cern.ch:/afs/cern.ch/work/j/johakala/private/HgammaSVN/notes/tmp/AN-16-125_temp.pdf ~/HgammaAN/drafts/AN_draft_${DATE}_v${ITERATION}.pdf
