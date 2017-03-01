077e46dc2643a0b9503b6f836664b85c836bbbe7
John Charles Hakala <johakala@kvm-s3562-1-ip149-13.cms>
2017-03-01 23:51:36 +0100

    make colors nicer

--- logViewer/contrast.js
+++ logViewer/contrast.js
@@ -1,4 +1,6 @@
 function setContrast() {
-  //$('span[style="background-color:#00CD00"]').css("color", "#ffffff");
-  $('span[style="background-color:#CD0000"]').css("color", "#eeeeee");
+  $('span[style="background-color:#00CD00"]').removeAttr("style").css("color", "#ffffff").css("background-color", "#00ae00");
+  $('span[style="background-color:#CD0000"]').removeAttr("style").css("color", "#f4f4f4").css("background-color", "#dd3844");
+  $('span[style="background-color:#CDCD00"]').removeAttr("style").css("background-color", "#e8e866");
+  $('span[style="background-color:#00CDCD"]').removeAttr("style").css("color", "#ffffff").css("background-color", "#4c77aa");
 }
