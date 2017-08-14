0cdac125c3c679a017f6dd5e0f6731570fe1efac
John Hakala <john_hakala@brown.edu>
2017-08-14 10:34:56 -0500

    refactor as step toward general-purpose applicability, add debugging help

---
+++ magicXmlParser/cfgBrickDelay.py
@@ -0,0 +1,59 @@
+from copy import deepcopy
+from magicXMLsax import CfgBrick
+from magicXMLutils import *
+
+# This class is a special implementation of a CfgBrick that specializes it to parsing RBX delay cfgBricks
+# TODO a class like this will be needed for each kind of magicXML
+class CfgBrickDelay(CfgBrick):
+  def __init__(self, outFileName):
+    info("Parsing magic xml of type: RBX Delays")
+    CfgBrick.__init__(self)
+    self.rbx = "test"
+    self.qieDelays = {}
+    self.tmpDict = {}
+    self.tmpKinds = ["RBX", "Data"]
+    self.outFileName = outFileName
+
+  def startElement(self, elementName, attributes):
+    if elementName == "Parameter":
+      if attributes.getValue("name") == "RBX":
+        self.tmpKind = "RBX"
+    if elementName == "Data":
+      self.tmpKind = "Data"
+      self.tmpDict = {"qie": attributes.getValue("qie"), "rm":attributes.getValue("rm")}
+    
+
+  def contentFiller(self, innerText):
+    if self.tmpKind in self.tmpKinds:
+      self.tmpContent = innerText
+    
+  def endElement(self, elementName):
+    if self.tmpKind == "RBX":
+      self.rbx = self.tmpContent
+      self.qieDelays[self.rbx]=[]
+      self.clearContent()
+
+    elif self.tmpKind == "Data":
+      self.tmpDict["delay"] = self.tmpContent
+      self.clearContent()
+      self.qieDelays[self.rbx].append(deepcopy(self.tmpDict))
+      self.tmpDict = {}
+
+  def endDocument(self):
+    from john_emapParser import emapper
+    # TODO make this settable via argument
+    emap = emapper("HCALmapHBHEP17_J.txt")
+    emap.parseEmap()
+
+    for channel in emap.listAll():
+      foundChannel = False
+      if channel["RBX"] in self.qieDelays.keys():
+        for qieDelay in self.qieDelays[channel["RBX"]]:
+          if channel["RM"] == qieDelay["rm"] and int(channel["card"])*int(channel["QIE"]) == int(qieDelay["qie"]):
+            channel["delay"] = qieDelay["delay"]
+            foundChannel = True
+      if not foundChannel:
+        emap.emap.remove(channel)
+
+    keepKey = "delay"
+    self.formatJson(keepKey, emap.emap)
--- magicXmlParser/magicXMLsax.py
+++ magicXmlParser/magicXMLsax.py
@@ -1,6 +1,6 @@
 from xml.sax import saxutils, handler, make_parser
 from os import path, makedirs
-from copy import deepcopy
+from magicXMLutils import error, info
 
 # John Hakala 8/10/17
 
@@ -8,8 +8,10 @@ from copy import deepcopy
 class CfgBrick():
   def __init__(self):
     self.elementType = "CFGBrick"
+    self.rbx = "test"
     self.tmpContent = ""
     self.tmpKind = "other"
+    self.tmpGen = "other"
 
   def clearContent(self):
     self.tmpContent = ""
@@ -27,78 +29,26 @@ class CfgBrick():
   def contentFiller(self, innerText):
     self.tmpContent = innerText
 
+  def endElement(self, elementName):
+    pass
+
   def endDocument(self):
     pass
 
-# This class is a special implementation of a CfgBrick that specializes it to parsing RBX delay cfgBricks
-# TODO a class like this will be needed for each kind of magicXML
-class CfgBrickDelay(CfgBrick):
-  def __init__(self, outFileName):
-    print "RBX Delays"
-    CfgBrick.__init__(self)
-    self.rbx = "test"
-    self.qieDelays = {}
-    self.tmpDict = {}
-    self.tmpKinds = ["RBX", "Data"]
-    self.outFileName = outFileName
-
-  def startElement(self, elementName, attributes):
-    if elementName == "Parameter":
-      if attributes.getValue("name") == "RBX":
-        self.tmpKind = "RBX"
-    if elementName == "Data":
-      self.tmpKind = "Data"
-      self.tmpDict = {"qie": attributes.getValue("qie"), "rm":attributes.getValue("rm")}
-    
-
-  def contentFiller(self, innerText):
-    if self.tmpKind in self.tmpKinds:
-      self.tmpContent = innerText
-    
-  def endElement(self, elementName):
-    if self.tmpKind == "RBX":
-      self.rbx = self.tmpContent
-      self.qieDelays[self.rbx]=[]
-      self.clearContent()
-
-    elif self.tmpKind == "Data":
-      self.tmpDict["delay"] = self.tmpContent
-      self.clearContent()
-      self.qieDelays[self.rbx].append(deepcopy(self.tmpDict))
-      self.tmpDict = {}
-
-  def endDocument(self):
-    from john_emapParser import emapper
-    # TODO make this settable via argument
-    emap = emapper("HCALmapHBHEP17_J.txt")
-    emap.parseEmap()
-
-    # TODO this could be moved to a function that isn't specific for RBX delays
-    # right now this is just where we stick the json formatting stuff
-    for channel in emap.listAll():
-      foundChannel = False
-      if channel["RBX"] in self.qieDelays.keys():
-        for qieDelay in self.qieDelays[channel["RBX"]]:
-          if channel["RM"] == qieDelay["rm"] and int(channel["card"])*int(channel["QIE"]) == int(qieDelay["qie"]):
-            channel["delay"] = qieDelay["delay"]
-            foundChannel = True
-      if not foundChannel:
-        emap.emap.remove(channel)
-
-    keepKeys = ["ieta", "iphi", "depth", "delay"]
-    for channel in emap.emap:
+  def formatJson(self, keepKey, emap):
+    keepKeys = ["ieta", "iphi", "depth", keepKey]
+    for channel in emap:
       for key in channel.keys():
         if not key in keepKeys:
           del channel[key]
+
     import json
     # TODO make this configurable
     outputDir = "outputJSONs"
     if not (path.exists(outputDir)):
       makedirs(outputDir)
     with open(path.join(outputDir, self.outFileName), "w") as outFile:
-     json.dump(emap.emap, outFile)
-      
-    
+     json.dump(emap, outFile)
   
 # this is meant to be a generic sax parser for all the different kind of magic xmls
 # to parse different types of magic xmls, you endow its' "self.brick" member object
@@ -115,13 +65,12 @@ class magicSAX(handler.ContentHandler):
   
   # this factory is used so that the handler can inherit specific 
   def CfgBrickFactory(self, infotype):
-    if infotype in self.validInfoTypes:
-      print "Parsing magicXML of type:",
-    else:
-      print "the infotype found was not valid:", infotype
+    if not infotype in self.validInfoTypes:
+      error("the infotype found was not valid: " + infotype)
 
     ## TODO this will need to be changed for all different kinds of magicXMLs
     if infotype == "DELAY":
+      from cfgBrickDelay import CfgBrickDelay
       self.brick = CfgBrickDelay(self.outFileName)
 
   def startElement(self, elementName, attributes):
@@ -139,6 +88,9 @@ class magicSAX(handler.ContentHandler):
     if self.brick.tmpKind == "INFOTYPE":
       self.CfgBrickFactory(self.brick.tmpContent)
       self.brick.clearContent()
+    if self.brick.tmpKind == "RBX_initial":
+      self.brick.rbx = self.brick.tmpContent
+      self.brick.clearContent()
     else:
       self.brick.endElement(elementName)
     self.brick.tmpKind = "other"
@@ -151,16 +103,18 @@ class magicSAX(handler.ContentHandler):
 def makeJSON(inputName, outFileName):
   from os.path import isfile, isdir
   if isfile(inputName):
-    inFile = inputName
-  elif isfile(inputName):
+    inFiles = [inputName]
+  elif isdir(inputName):
     inDir = inputName
-    print "parsing directories of xmls is not yet supported"
-    exit(1)
+    from glob import glob
+    inFiles = glob("{}/*.xml".format(inputName))
+    
   else:
-    print "the input specified does not seem to be a valid file or directory:", inputName
+    error("the input specified does not seem to be a valid file or directory: " + inputName)
      
   saxParser = make_parser()
-  print "outFileName is:", outFileName
+  info("outFileName is: " + outFileName)
   saxParser.setContentHandler(magicSAX(outFileName))
-  saxParser.parse(inFile)
+  for inFile in inFiles:
+    saxParser.parse(inFile)
 
