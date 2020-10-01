f4f6915e6c76bdab3cecc476ee256f8245b5275e
John Hakala <john_hakala@virginia.edu>
2020-10-01 17:30:58 -0500

    tail only one cluster if given

--- tailall.sh
+++ tailall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in `condor_q | grep ' R ' | awk {'print $1'} `; do echo ${i} ; condor_tail ${i} ; condor_tail ${i} -stderr; echo -e "\n------------------------------------\n\n"; done
+for i in `condor_q $1 | grep ' R ' | awk {'print $1'} `; do echo ${i} ; timeout 5 condor_tail ${i} ; timeout 5 condor_tail ${i} -stderr; echo -e "\n------------------------------------\n\n"; done
